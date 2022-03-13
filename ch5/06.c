b7b942891fd7541fe84277d1c74502c98573b226
Mark Curtiss <markcurtiss@gmail.com>
2022-03-13 11:01:52 -0700

    work problem 6

---
+++ ch5/06.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+int
+main()
+{
+    printf("hello world (pid:%d)\n", (int) getpid());
+
+    int rc = fork();
+    if (rc < 0) {
+        // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        // child (new process)
+        printf("hello, I am child (pid:%d) \n", (int) getpid());
+    } else {
+        // parent goes down this path (original process)
+        printf("hello, I am parent of %d (pid:%d)\n",
+	       rc, (int) getpid());
+        int status;
+        if (waitpid(0, &status, 0) >= 0) {
+            printf("child process exited with status %d\n", status);
+        };
+    }
+    return 0;
+}
+
+/*
+waitpid() would be useful when we want to wait on a specific process, for example,
+one that's doing network I/O
+*/
